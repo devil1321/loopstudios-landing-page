776b87b846673695f833a3abb6dc6f0ebb1efa57
devil1321 <s.dominik1321@gmail.com>
2021-01-26 15:55:54 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
 const navBtn = document.querySelector('.nav__button');
+const navLinks = document.querySelectorAll('.nav__link');
+const navItems = document.querySelectorAll('.nav__item');
 const nav = document.querySelector('.nav');
 const menu = document.querySelector('.nav__list-wrapper');
+const heroText = document.querySelector('.hero__text');
 
 window.addEventListener('resize', function() {
-    if (window.innerWidth <= 375) {
+    if (window.innerWidth <= 1040) {
         menu.style.width = "0%";
         menu.style.display = "none";
     }
-    if (window.innerWidth > 375) {
+    if (window.innerWidth > 1040) {
         menu.style.width = "40%";
         menu.style.display = "flex";
     }
+    if (window.innerHeight <= 375) {
+        navLinks.forEach(link => {
+            link.style.fontSize = "1rem";
+        })
+        navItems.forEach(item => {
+            item.style.margin = "1rem 0rem";
+        })
 
+    } else {
+        navLinks.forEach(link => {
+            link.style.fontSize = "2rem";
+        })
+        navItems.forEach(item => {
+            item.style.margin = " 1.25rem 0rem"
+        })
+    }
+    if (window.innerHeight <= 320) {
+        heroText.style.marginTop = "6.60rem";
+    } else {
+        heroText.style.marginTop = "6.25rem";
+    }
+    if (window.innerWidth === 1366 && window.innerHeight === 1024) {
+        navLinks.forEach(link => {
+            link.style.fontSize = "1rem";
+        })
+    }
 })
 
 
