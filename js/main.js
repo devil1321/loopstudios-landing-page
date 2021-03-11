5f23e98f8b2570d4db6f217578bd260b2410799e
devil1321 <s.dominik1321@gmail.com>
2021-03-11 12:32:53 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -4,15 +4,32 @@ const navItems = document.querySelectorAll('.nav__item');
 const nav = document.querySelector('.nav');
 const menu = document.querySelector('.nav__list-wrapper');
 const heroText = document.querySelector('.hero__text');
+const body = document.querySelector('body')
+const navHeader = document.querySelector('.nav__header')
 
 window.addEventListener('resize', function() {
+    if(window.innerWidth === 320 && window.innerHeight === 568){
+        navHeader.style.padding = '0.5625rem'
+        heroText.style.fontSize = '2rem'
+    }else{
+        heroText.style.fontSize = '2.5rem'
+    }
     if (window.innerWidth <= 1040) {
         menu.style.width = "0%";
         menu.style.display = "none";
     }
-    if (window.innerWidth > 1040) {
+    if (window.innerWidth >= 1040) {
         menu.style.width = "40%";
         menu.style.display = "flex";
+        nav.style.position = 'absolute'
+        body.style.position = 'static'
+        navLinks.forEach(link =>{
+            link.style.fontSize = '0.9375rem'
+        })
+    }else{
+         navLinks.forEach(link => {
+            link.style.fontSize = "2rem";
+        })
     }
     if (window.innerHeight <= 375) {
         navLinks.forEach(link => {
@@ -23,9 +40,7 @@ window.addEventListener('resize', function() {
         })
 
     } else {
-        navLinks.forEach(link => {
-            link.style.fontSize = "2rem";
-        })
+       
         navItems.forEach(item => {
             item.style.margin = " 1.25rem 0rem"
         })
@@ -50,10 +65,17 @@ function navToggle() {
         nav.style.backgroundColor = "black";
         menu.style.display = "flex";
         menu.style.transition = "all 1s ease-in-out";
+        nav.style.position = 'fixed'
+        body.style.position = 'fixed'
+        nav.style.height = '100vh'
     } else {
         menu.style.width = "0%";
         nav.style.backgroundColor = "transparent";
         menu.style.transition = "all 200ms ease-in-out";
+        nav.style.position = 'absolute'
+        body.style.position = 'static'
+        nav.style.height = '100px'
+
     }
 }
 navBtn.addEventListener('click', navToggle);
